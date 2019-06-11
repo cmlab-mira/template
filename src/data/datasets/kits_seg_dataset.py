4d4753660ada80d9ce163bcad55b56ff40e50fe4
vic85821 <vic85821@gmail.com>
2019-06-11 17:05:42 +0800

    Update the kits seg dataset and the config

--- src/data/datasets/kits_seg_dataset.py
+++ src/data/datasets/kits_seg_dataset.py
@@ -2,6 +2,7 @@ import csv
 import glob
 import torch
 import numpy as np
+from box import Box
 import nibabel as nib
 from pathlib import Path
 
@@ -15,10 +16,11 @@ class KitsSegDataset(BaseDataset):
         data_split_csv (str): the csv path of the training / validation data split file
         transforms (Box): the preprocessing and augmentation techiques applied to the data
     """
-    def __init__(self, data_split_csv, transforms, **kwargs):
+    def __init__(self, data_split_csv, train_transforms, valid_transforms, **kwargs):
         super().__init__(**kwargs)
         self.data_split_csv = data_split_csv
-        self.transforms = compose(transforms)
+        self.train_transforms = compose(train_transforms)
+        self.valid_transforms = compose(valid_transforms)
         self.image_path, self.label_path = [], []
         self.data = []
 
@@ -48,7 +50,12 @@ class KitsSegDataset(BaseDataset):
         image, label = nib.load(str(self.image_path[path_index])).get_data(), nib.load(str(self.label_path[path_index])).get_data()
         image, label = image.astype(np.float64), label.astype(np.uint8)
         image, label = image[slice_index:slice_index+1].transpose((1, 2, 0)), label[slice_index:slice_index+1].transpose((1, 2, 0))
-        image, label = self.transforms(image, label, normalize_tags=[True, False], dtypes=[torch.float, torch.long])
+
+        if self.type == 'train':
+            image, label = self.train_transforms(image, label, normalize_tags=[True, False], dtypes=[torch.float, torch.long])
+        elif self.type == 'valid':
+            image, label = self.valid_transforms(image, label, normalize_tags=[True, False], dtypes=[torch.float, torch.long])
+
         image, label = image.permute(2, 0, 1).contiguous(), label.permute(2, 0, 1).contiguous()
 
         return {"image": image, "label": label}
