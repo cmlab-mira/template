25f7c2c9ca4b4dc6dd1fcfedaa8bba29a0c82678
peter850706 <peter850706@gmail.com>
2019-06-12 17:09:54 +0800

    Modify KitsClfSegDataset transforms

--- src/data/datasets/kits_clf_seg_dataset.py
+++ src/data/datasets/kits_clf_seg_dataset.py
@@ -14,14 +14,16 @@ class KitsClfSegDataset(BaseDataset):
     Args:
         data_split_csv (str): The path of the training and validation data split csv file.
         task (str): The task name ('clf' or 'seg').
-        transforms (Box): The preprocessing and augmentation techiques applied to the data.
+        train_transforms (Box): The preprocessing and augmentation techiques applied to the training data.
+        valid_transforms (Box): The preprocessing and augmentation techiques applied to the validation data.
     """
-    def __init__(self, data_split_csv, task, transforms, **kwargs):
+    def __init__(self, data_split_csv, task, train_transforms, valid_transforms, **kwargs):
         super().__init__(**kwargs)
         self.data_split_csv = data_split_csv
         self.task = task
-        self.image_transforms = compose(transforms) # for images and 'seg' labels
-        self.label_transforms = compose() # for 'clf' labels
+        self.train_transforms = compose(train_transforms) # for training images and 'seg' labels
+        self.valid_transforms = compose(valid_transforms) # for validation images
+        self.clf_label_transforms = compose() # for 'clf' labels
         self.data_paths = []
 
         # Collect the data paths according to the dataset_split_csv.
@@ -45,11 +47,18 @@ class KitsClfSegDataset(BaseDataset):
     def __getitem__(self, index):
         image_path, label_path = self.data_paths[index]
         image, label = nib.load(str(image_path)).get_data(), nib.load(str(label_path)).get_data()
-        if self.task == 'clf':
-            image = self.image_transforms(image, dtypes=[torch.float])
-            label = self.label_transforms(label, dtypes=[torch.long])
+        if self.type == 'train' and self.task == 'clf':
+            image = self.train_transforms(image, dtypes=[torch.float])
+            label = self.clf_label_transforms(label, dtypes=[torch.long])
             image = image.permute(2, 0, 1).contiguous()
-        elif self.task == 'seg':
-            image, label = self.image_transforms(image, label, normalize_tags=[True, False], dtypes=[torch.float, torch.long])
+        elif self.type == 'valid' and self.task == 'clf':
+            image = self.valid_transforms(image, dtypes=[torch.float])
+            label = self.clf_label_transforms(label, dtypes=[torch.long])
+            image = image.permute(2, 0, 1).contiguous()
+        elif self.type == 'train' and self.task == 'seg':
+            image, label = self.train_transforms(image, label, normalize_tags=[True, False], dtypes=[torch.float, torch.long])
+            image, label = image.permute(2, 0, 1).contiguous(), label.permute(2, 0, 1).contiguous()
+        elif self.type == 'valid' and self.task == 'seg':
+            image, label = self.valid_transforms(image, label, normalize_tags=[True, False], dtypes=[torch.float, torch.long])
             image, label = image.permute(2, 0, 1).contiguous(), label.permute(2, 0, 1).contiguous()
         return {"image": image, "label": label}
