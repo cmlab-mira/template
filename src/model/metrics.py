8d5925472dfe3d1273ebe80706fb8124de8a4102
peter850706 <peter850706@gmail.com>
2019-06-14 21:35:37 +0800

    Update metric.py

--- src/model/metrics.py
+++ src/model/metrics.py
@@ -1,37 +1,49 @@
 import torch
 import torch.nn as nn
-import numpy as np
 
 
 class Dice(nn.Module):
-    """Dice score
-    Args:
-        num_classes (int): the number of the prediction class
-    """
-    def __init__(self, num_classes):
-        super().__init__()
-        self.num_classes = num_classes
-
-    def forward(self, pred, label):
-        # One hot the prediction and the ground truth label
-        _pred = torch.zeros((pred.shape[0], self.num_classes, *pred.shape[2:])).to(pred.device).scatter_(1, pred, 1)
-        _label = torch.zeros_like(_pred).scatter_(1, label, 1)
-
-        # Calculate the dice score
-        reduce_dim = tuple(np.arange(0, len(pred.shape))[2:])
-        intersection = 2.0 * (_pred * _label).sum(reduce_dim)
-        union = (_pred).sum(reduce_dim) + (_label).sum(reduce_dim)
-        epsilon = 1e-10
-        score = intersection / (union + epsilon)
-        return torch.mean(score, dim=0)
-
-
-class Accuracy(nn.Module):
-    """The accuracy for the classification.
+    """The Dice score.
     """
     def __init__(self):
         super().__init__()
 
-    def forward(self, output, batch):
+    def forward(self, output, target):
+        """
+        Args:
+            output (torch.Tensor) (N, C, *): The model output.
+            target (torch.Tensor) (N, 1, *): The data target.
+
+        Returns:
+            metric (torch.Tensor) (C): The dice scores for each class.
+        """
+        # Get the one-hot encoding of the prediction and the ground truth label.
+        pred = output.argmax(dim=1, keepdim=True)
+        pred = torch.zeros_like(output).scatter_(1, pred, 1)
+        target = torch.zeros_like(output).scatter_(1, target, 1)
+
+        # Calculate the dice score.
+        reduced_dims = list(range(2, output.dim())) # (N, C, *) --> (N, C)
+        intersection = 2.0 * (pred * target).sum(reduced_dims)
+        union = pred.sum(reduced_dims) + target.sum(reduced_dims)
+        score = intersection / (union + 1e-10)
+        return score.mean(dim=0)
+
+
+class Accuracy(nn.Module):
+    """The accuracy for the classification task.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, output, target):
+        """
+        Args:
+            output (torch.Tensor) (N, C): The model output.
+            target (torch.Tensor) (N): The data target.
+
+        Returns:
+            metric (torch.Tensor) (0): The accuracy.
+        """
         pred = torch.argmax(output, dim=1)
-        return (pred == batch['label']).float().mean()
+        return (pred == target).float().mean()
