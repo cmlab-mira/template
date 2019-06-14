884b40e673819f3b41e70c8f8001652d30c78f2b
peter850706 <peter850706@gmail.com>
2019-06-14 22:55:03 +0800

    Update KitsSegLogger comments

--- src/callbacks/loggers/kits_seg_logger.py
+++ src/callbacks/loggers/kits_seg_logger.py
@@ -6,10 +6,10 @@ from .base_logger import BaseLogger
 
 
 class KitsSegLogger(BaseLogger):
-
+    """The KiTS logger for the segmentation task.
+    """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
 
     def _add_images(self, epoch, train_batch, train_output, valid_batch, valid_output):
         """Plot the visualization results.
