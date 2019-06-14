8fec3234a098e78df45f4986f189f10b59c19610
peter850706 <peter850706@gmail.com>
2019-06-14 22:56:45 +0800

    Modify KitsSegDataset

--- src/data/datasets/kits_seg_dataset.py
+++ src/data/datasets/kits_seg_dataset.py
@@ -11,46 +11,38 @@ from src.data.transforms import compose
 
 
 class KitsSegDataset(BaseDataset):
-    """ 2019 MICCAI challenge (ref: https://kits19.grand-challenge.org/). The kits dataset for two stage methods.
+    """The Kidney Tumor Segmentation (KiTS) Challenge dataset (ref: https://kits19.grand-challenge.org/) for the 2D segmentation method (do not exclude the slices that does not contain the kidney or the tumer).
     Args:
-        data_split_csv (str): the csv path of the training / validation data split file
-        train_transforms (Box): the preprocessing and augmentation techiques applied to the training data
-        valid_transforms (Box): the preprocessing and augmentation techiques applied to the validation data
+        data_split_csv (str): The path of the training and validation data split csv file.
+        train_transforms (Box): The preprocessing and augmentation techiques applied to the training data.
+        valid_transforms (Box): The preprocessing and augmentation techiques applied to the validation data.
     """
     def __init__(self, data_split_csv, train_transforms, valid_transforms, **kwargs):
         super().__init__(**kwargs)
         self.data_split_csv = data_split_csv
         self.train_transforms = compose(train_transforms)
         self.valid_transforms = compose(valid_transforms)
-        self.image_path, self.label_path = [], []
-        self.data = []
+        self.data_paths = []
 
-        # Collect the data paths according to the dataset split csv
+        # Collect the data paths according to the dataset split csv.
         with open(self.data_split_csv, "r") as f:
-            split_type = 'Training' if self.type == 'train' else 'Validation'
+            type_ = 'Training' if self.type == 'train' else 'Validation'
             rows = csv.reader(f)
-            for row in rows:
-                if row[1] == split_type:
-                    image_paths = [path for path in (self.data_root / row[0]).iterdir() \
-                            if path.is_file() and 'imaging' in path.parts[-1]]
-                    label_paths = [path for path in (self.data_root / row[0]).iterdir() \
-                            if path.is_file() and 'segmentation' in path.parts[-1]]
-                    self.image_path.extend(image_paths)
-                    self.label_path.extend(label_paths)
+            for case_name, split_type in rows:
+                if split_type == type_:
+                    _image_paths = sorted(list((self.data_root / case_name).glob('imaging*.nii.gz')))
+                    _label_paths = sorted(list((self.data_root / case_name).glob('segmentation*.nii.gz')))
+                    self.data_paths.extend([(image_path, label_path) for image_path, label_path in zip(_image_paths, _label_paths)])
 
     def __len__(self):
-        return len(self.image_path)
+        return len(self.data_paths)
 
     def __getitem__(self, index):
-        image, label = nib.load(str(self.image_path[index])).get_data(), nib.load(str(self.label_path[index])).get_data()
-        image, label = image.transpose((1, 2, 0)), label.transpose((1, 2, 0))
-        image, label = image.astype(np.float64), label.astype(np.uint8)
-
+        image_path, label_path = self.data_paths[index]
+        image, label = nib.load(str(image_path)).get_data(), nib.load(str(label_path)).get_data()
         if self.type == 'train':
             image, label = self.train_transforms(image, label, normalize_tags=[True, False], dtypes=[torch.float, torch.long])
         elif self.type == 'valid':
             image, label = self.valid_transforms(image, label, normalize_tags=[True, False], dtypes=[torch.float, torch.long])
-
         image, label = image.permute(2, 0, 1).contiguous(), label.permute(2, 0, 1).contiguous()
-
         return {"image": image, "label": label}
