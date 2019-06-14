e1fa4a38806ffd7fcf749378219cf53ea6564a62
peter850706 <peter850706@gmail.com>
2019-06-14 21:33:54 +0800

    Modify KitsSegTrainer

--- src/runner/trainers/kits_seg_trainer.py
+++ src/runner/trainers/kits_seg_trainer.py
@@ -6,74 +6,42 @@ from src.runner.trainers.base_trainer import BaseTrainer
 
 
 class KitsSegTrainer(BaseTrainer):
+    """The KiTS trainer for segmentation task.
+    """
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def _run_epoch(self, mode):
-        """Run an epoch for training.
+    def _set_inputs_targets(self, batch):
+        """Specify the data input and target.
         Args:
-            mode (str): The mode of running an epoch ('training' or 'validation').
+            batch (dict or tuple): A batch of data.
         Returns:
-            log (dict): The log information.
-            batch (dict or tuple): The last batch of the data.
-            output (torch.Tensor): The corresponding output.
+            input (torch.Tensor): The data input.
+            target (torch.Tensor): The data target.
         """
-        if mode == 'training':
-            self.net.train()
-        else:
-            self.net.eval()
-        dataloader = self.train_dataloader if mode == 'training' else self.valid_dataloader
-        trange = tqdm(dataloader,
-                      total=len(dataloader),
-                      desc=mode)
+        return batch['image'], batch['label']
 
-        log = self._init_log()
-        count = 0
-        for batch in trange:
-            if isinstance(batch, dict):
-                batch = dict((key, data.to(self.device)) for key, data in batch.items())
-            else:
-                batch = tuple(data.to(self.device) for data in batch)
+    def _compute_losses(self, output, target):
+        """Compute the losses.
+        Args:
+            output (torch.Tensor): The model output.
+            target (torch.Tensor): The data target.
+        Returns:
+            losses (sequence of torch.Tensor): The computed losses.
+        """
+        losses = [loss(output, target) for loss in self.losses]
+        return losses
 
-            if mode == 'training':
-                output, losses = self._run_iter(batch)
-                loss = (torch.stack(losses) * self.loss_weights).sum()
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
-            else:
-                with torch.no_grad():
-                    output, losses = self._run_iter(batch)
-                    loss = (torch.stack(losses) * self.loss_weights).sum()
-
-            if self.lr_scheduler is None:
-                pass
-            elif isinstance(self.lr_scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau) and mode == 'validation':
-                self.lr_scheduler.step(loss)
-            else:
-                self.lr_scheduler.step()
-
-            batch_size = output.size(0)
-            log['Loss'] += loss.item() * batch_size
-            for loss, _loss in zip(self.losses, losses):
-                log[loss.__class__.__name__] += _loss.item() * batch_size
-            for metric in self.metrics:
-                scores = metric(output.argmax(dim=1, keepdim=True), batch['label'])
-                for i, score in enumerate(scores):
-                    log[f'{metric.__class__.__name__}_{i}'] += score.item() * batch_size
-            count += batch_size
-            trange.set_postfix(**dict((key, value / count) for key, value in log.items()))
-
-        for key in log:
-            log[key] /= count
-        return log, batch, output
-
-    def _run_iter(self, batch):
-        image, label = batch['image'], batch['label']
-        pred = self.net(image)
-        losses = tuple(loss(pred, label) for loss in self.losses)
-
-        return pred, losses
+    def _compute_metrics(self, output, target):
+        """Compute the metrics.
+        Args:
+             output (torch.Tensor): The model output.
+             target (torch.Tensor): The data target.
+        Returns:
+            metrics (sequence of torch.Tensor): The computed metrics.
+        """
+        metrics = [metric(output, target) for metric in self.metrics]
+        return metrics
 
     def _init_log(self):
         """Initialize the log.
@@ -85,6 +53,30 @@ class KitsSegTrainer(BaseTrainer):
         for loss in self.losses:
             log[loss.__class__.__name__] = 0
         for metric in self.metrics:
-            for i in range(self.net.out_channels):
-                log[f'{metric.__class__.__name__}_{i}'] = 0
+            if metric.__class__.__name__ == 'Dice':
+                log['Dice'] = 0
+                for i in range(self.net.out_channels):
+                    log[f'Dice_{i}'] = 0
+            else:
+                log[metric.__class__.__name__] = 0
         return log
+
+    def _update_log(self, log, batch_size, loss, losses, metrics):
+        """Update the log.
+        Args:
+            log (dict): The log to be updated.
+            batch_size (int): The batch size.
+            loss (torch.Tensor): The weighted sum of the computed losses.
+            losses (sequence of torch.Tensor): The computed losses.
+            metrics (sequence of torch.Tensor): The computed metrics.
+        """
+        log['Loss'] += loss.item() * batch_size
+        for loss, _loss in zip(self.losses, losses):
+            log[loss.__class__.__name__] += _loss.item() * batch_size
+        for metric, _metric in zip(self.metrics, metrics):
+            if metric.__class__.__name__ == 'Dice':
+                log['Dice'] += _metric.mean().item() * batch_size
+                for i, class_score in enumerate(_metric):
+                    log[f'Dice_{i}'] += class_score.item() * batch_size
+            else:
+                log[metric.__class__.__name__] += _metric.item() * batch_size
