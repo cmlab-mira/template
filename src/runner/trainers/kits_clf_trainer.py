5045d671c67e4ccab2d27ff9c06f9364cc3b79c3
peter850706 <peter850706@gmail.com>
2019-06-14 22:55:44 +0800

    Modify KitsClfTrainer

--- src/runner/trainers/kits_clf_trainer.py
+++ src/runner/trainers/kits_clf_trainer.py
@@ -9,16 +9,36 @@ class KitsClfTrainer(BaseTrainer):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def _run_iter(self, batch):
-        """Run an iteration to obtain the output and the losses.
+    def _set_inputs_targets(self, batch):
+        """Specify the data input and target.
         Args:
             batch (dict or tuple): A batch of data.
         Returns:
-            output (torch.Tensor): The computed output.
-            losses (sequence of torch.Tensor): The computed losses.
+            input (torch.Tensor): The data input.
+            target (torch.Tensor): The data target.
         """
         image, label = batch['image'], batch['label']
         image = torch.cat([image, image, image], dim=1) # Concatenate three one-channel images to a three-channels image.
-        output = self.net(image)
-        losses = tuple(loss(output, label) for loss in self.losses)
-        return output, losses
+        return image, label
+
+    def _compute_losses(self, output, target):
+        """Compute the losses.
+        Args:
+            output (torch.Tensor): The model output.
+            target (torch.Tensor): The data target.
+        Returns:
+            losses (sequence of torch.Tensor): The computed losses.
+        """
+        losses = [loss(output, target) for loss in self.losses]
+        return losses
+
+    def _compute_metrics(self, output, target):
+        """Compute the metrics.
+        Args:
+             output (torch.Tensor): The model output.
+             target (torch.Tensor): The data target.
+        Returns:
+            metrics (sequence of torch.Tensor): The computed metrics.
+        """
+        metrics = [metric(output, target) for metric in self.metrics]
+        return metrics
